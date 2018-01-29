9d31ed45f096d8c83ef2aed63657903ba41cce68
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-01-29 01:52:40 -0300

    Cria serviço de autenticação

---
+++ src/app/authentication/auth.service.ts
@@ -0,0 +1,19 @@
+﻿import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+@Injectable()
+export class AuthService {
+    constructor(public afAuth: AngularFireAuth) { }
+    login() {
+        this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+        // Verifica email
+        // this.dmUsers.checkEmail(afAuth.auth.currentUser.email)
+    }
+    logout() {
+        return this.afAuth.auth.signOut();
+    }
+    authState() {
+        return this.afAuth.authState;
+    }
+}
