0bdb61547c201b8f18067e1f52afd13f87217ddc
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-01-29 01:53:36 -0300

    Cria componente de login page

---
+++ src/app/authentication/login/login.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+    selector: 'app-login-page',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class LoginPageComponent implements OnInit {
+    constructor(public authService: AuthService, private router: Router) { }
+    ngOnInit() {
+    }
+    login() {
+        this.authService.login();
+        this.router.navigate(['']); 
+    }
+}
