460a7ea34e1441e2787ce657a6ee47d804488e2e
LucasAurelio <lucas.lima@ccc.ufcg.edu.br>
2018-02-06 15:52:37 -0300

    Deploy to firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: 'AIzaSyDq5wJTcjPWrKkfESxAnmrKlH6i4izzYeE',
     authDomain: 'alocca-91315.firebaseapp.com',
